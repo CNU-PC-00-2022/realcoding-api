b60c40abee6ec418f7c8424e152cb25a111b6de3
Seo, Yeong-Hak <seo.yeonghak.1992@gmail.com>
2022-04-08 23:19:42 +0900

    progress - step 2. mvc architecture

--- src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
@@ -1,13 +1,30 @@
 package ac.cnu.realcodingapi.controller;
 
+import ac.cnu.realcodingapi.dto.UserGroupResponse;
+import ac.cnu.realcodingapi.service.UserGroupService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UserGroupController {
+
+    private final UserGroupService userGroupService;
+
+    public UserGroupController(UserGroupService userGroupService) {
+        this.userGroupService = userGroupService;
+    }
+
     @GetMapping("/api")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/api/usergroup")
+    public ResponseEntity<UserGroupResponse> getUserGroup(@RequestParam Long id) {
+        return ResponseEntity.ok().body(userGroupService.getUserGroup(id));
     }
 }
--- src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
@@ -1,7 +1,22 @@
 package ac.cnu.realcodingapi.service;
 
+import ac.cnu.realcodingapi.domain.UserGroup;
+import ac.cnu.realcodingapi.domain.UserGroupDomainService;
+import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserGroupService {
+
+    private final UserGroupDomainService userGroupDomainService;
+
+    public UserGroupService(UserGroupDomainService userGroupDomainService) {
+        this.userGroupDomainService = userGroupDomainService;
+    }
+
+    public UserGroupResponse getUserGroup(Long id) {
+        UserGroup userGroup = userGroupDomainService.getUserGroup(id);
+
+        return UserGroupResponse.create(userGroup);
+    }
 }
