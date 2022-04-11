06eb2d6ed70c2e009ebfb7312637f8920ba4aef8
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-04-12 04:02:10 +0900

    [Feature] Programmed MVC model by spring

--- src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
@@ -1,7 +1,25 @@
 package ac.cnu.realcodingapi.service;
 
+import ac.cnu.realcodingapi.domain_model.UserGroup;
+import ac.cnu.realcodingapi.domain_model.UserGroupDomainService;
+import ac.cnu.realcodingapi.dto.UserGroupResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserGroupService {
+
+    private final UserGroupDomainService userGroupDomainService;
+
+    public UserGroupService(UserGroupDomainService userGroupDomainService) {
+        this.userGroupDomainService = userGroupDomainService;
+    }
+
+    public UserGroupResponse getUserGroup(Long id) {
+        log.info("id로 usergroup 가져오기");
+        UserGroup userGroup = userGroupDomainService.getUserGroup(id);
+
+        return UserGroupResponse.create(userGroup);
+    }
 }
