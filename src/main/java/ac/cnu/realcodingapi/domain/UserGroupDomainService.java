dc939e38d9c40216b8df9bfc11a1aa071db48874
zhzzang <leejh0799@gmail.com>
2022-04-11 22:57:46 +0900

    feat: 계층별 로깅 기능 구현

--- src/main/java/ac/cnu/realcodingapi/domain/UserGroupDomainService.java
+++ src/main/java/ac/cnu/realcodingapi/domain/UserGroupDomainService.java
@@ -1,11 +1,13 @@
 package ac.cnu.realcodingapi.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -15,15 +17,18 @@ public class UserGroupDomainService {
 
     @Transactional
     public UserGroup saveUserGroup(UserGroup userGroup) {
+        log.info("UserGroupDomainService layer 시작");
         return userGroupRepository.save(userGroup);
     }
 
     public UserGroup getUserGroup(long id) {
+        log.info("UserGroupDomainService layer 시작");
         return userGroupRepository.findById(id)
                 .orElseGet(() -> new UserGroup("no group"));
     }
 
     public List<UserGroup> getUserGroups() {
+        log.info("UserGroupDomainService layer 시작");
         return userGroupRepository.findAll();
     }
 }
--- src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
@@ -5,27 +5,32 @@ import ac.cnu.realcodingapi.domain.UserGroupDomainService;
 import ac.cnu.realcodingapi.dto.UserGroupRequest;
 import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserGroupService {
     private final UserGroupDomainService userGroupDomainService;
 
     public UserGroupResponse saveUserGroup(UserGroupRequest request) {
+        log.info("UserGroupService layer 시작");
         UserGroup userGroup = userGroupDomainService.saveUserGroup(request.toUserGroup());
         return UserGroupResponse.create(userGroup);
     }
 
     public UserGroupResponse getUserGroup(Long id) {
+        log.info("UserGroupService layer 시작");
         UserGroup userGroup = userGroupDomainService.getUserGroup(id);
         return UserGroupResponse.create(userGroup);
     }
 
     public List<UserGroupResponse> getUserGroups() {
+        log.info("UserGroupService layer 시작");
         return userGroupDomainService.getUserGroups().stream()
                 .map(UserGroupResponse::create)
                 .collect(Collectors.toList());
