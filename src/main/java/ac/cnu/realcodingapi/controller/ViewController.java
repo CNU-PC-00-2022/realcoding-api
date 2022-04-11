5133b255755950062d6b6d583c6177420f617cda
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2022-04-11 17:37:04 +0900

    team-3 : assignment

--- src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
@@ -1,14 +1,17 @@
 package ac.cnu.realcodingapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class ViewController {
     @GetMapping("/index")
     public String index(Model model) {
+        log.info("view group controller입니다. ");
         model.addAttribute("group", 10);
         return "index";
     }
