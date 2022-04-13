8d58bc6bc4fecfa0ad03208c8810269b0d858b31
BaekMinHyeon <201802101@o.cnu.ac.kr>
2022-04-13 18:26:21 +0900

    Team 4 - assignment

--- src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
@@ -1,13 +1,33 @@
 package ac.cnu.realcodingapi.controller;
 
+import ac.cnu.realcodingapi.dto.UserGroupResponse;
+import ac.cnu.realcodingapi.service.UserGroupService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class UserGroupController {
+
+    private final UserGroupService userGroupService;
+
+    public UserGroupController(UserGroupService userGroupService) {
+        this.userGroupService = userGroupService;
+    }
+
     @GetMapping("/api")
     public String index() {
+        log.info("user group api controller입니다");
         return "index";
+    }
+
+
+    @GetMapping("/api/usergroup")
+    public ResponseEntity<UserGroupResponse> getUserGroup(@RequestParam Long id) {
+        log.info("user group controller입니다");
+        return ResponseEntity.ok().body(userGroupService.getUserGroup(id));
     }
 }
--- src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
@@ -1,15 +1,18 @@
 package ac.cnu.realcodingapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ViewController {
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("group", 10);
+        log.info("view index controller입니다");
+        model.addAttribute("group", 4);
         return "index";
     }
 }
