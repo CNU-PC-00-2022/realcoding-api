5beda22e34fb08b52ecec714ceafd77a7cc9d8b0
gomoji <beyond58@nate.com>
2022-04-15 05:09:07 +0900

    feat : function add

--- src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/ViewController.java
@@ -1,15 +1,17 @@
 package ac.cnu.realcodingapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class ViewController {
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("group", 10);
+        log.info("controller");
+        model.addAttribute("group", 15);
         return "index";
     }
 }
--- src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
@@ -1,7 +1,26 @@
 package ac.cnu.realcodingapi.service;
-
+import ac.cnu.realcodingapi.domain.UserGroup;
+import lombok.extern.slf4j.Slf4j;
+import ac.cnu.realcodingapi.domain.UserGroupDomainService;
+import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import org.springframework.stereotype.Service;
 
+
+
+@Slf4j
 @Service
 public class UserGroupService {
+
+    private final UserGroupDomainService userGroupDomainService;
+
+    public UserGroupService(UserGroupDomainService userGroupDomainService) {
+        this.userGroupDomainService = userGroupDomainService;
+    }
+
+    public UserGroupResponse getUserGroup(Long id) {
+        log.info("service start");
+        UserGroup userGroup = userGroupDomainService.getUserGroup(id);
+
+        return UserGroupResponse.create(userGroup);
+    }
 }
