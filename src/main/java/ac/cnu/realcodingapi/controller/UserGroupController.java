44bcb795033d16be5fcbaa41729bbba34f081d51
Yongil Kim <yongil.k01@gmail.com>
2022-04-16 17:01:25 +0900

    Second log

--- src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
@@ -2,13 +2,14 @@ package ac.cnu.realcodingapi.controller;
 
 import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import ac.cnu.realcodingapi.service.UserGroupService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class UserGroupController {
 
@@ -25,6 +26,7 @@ public class UserGroupController {
 
     @GetMapping("/api/usergroup")
     public ResponseEntity<UserGroupResponse> getUserGroup(@RequestParam Long id) {
+        log.info("컨트롤러입니다.");
         return ResponseEntity.ok().body(userGroupService.getUserGroup(id));
     }
 }
--- src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupService.java
@@ -3,8 +3,10 @@ package ac.cnu.realcodingapi.service;
 import ac.cnu.realcodingapi.domain.UserGroup;
 import ac.cnu.realcodingapi.domain.UserGroupDomainService;
 import ac.cnu.realcodingapi.dto.UserGroupResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserGroupService {
 
@@ -15,8 +17,8 @@ public class UserGroupService {
     }
 
     public UserGroupResponse getUserGroup(Long id) {
+        log.info("서비스입니다.");
         UserGroup userGroup = userGroupDomainService.getUserGroup(id);
-
         return UserGroupResponse.create(userGroup);
     }
 }
