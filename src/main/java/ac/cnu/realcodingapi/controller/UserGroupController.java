cdd2d4950569d16d03d70e12bacc5e4b42517d4c
giwhan-kim <kgh06079@naver.com>
2022-04-17 00:52:36 +0900

    assignment/team-12

--- src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
+++ src/main/java/ac/cnu/realcodingapi/controller/UserGroupController.java
@@ -2,13 +2,13 @@ package ac.cnu.realcodingapi.controller;
 
 import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import ac.cnu.realcodingapi.service.UserGroupService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class UserGroupController {
 
@@ -23,8 +23,11 @@ public class UserGroupController {
         return "index";
     }
 
+    // 과제1 Get 매핑 함수
     @GetMapping("/api/usergroup")
     public ResponseEntity<UserGroupResponse> getUserGroup(@RequestParam Long id) {
+        log.info("Controller 함수 시작");
         return ResponseEntity.ok().body(userGroupService.getUserGroup(id));
     }
+
 }
--- src/main/java/ac/cnu/realcodingapi/domain/UserGroupDomainService.java
+++ src/main/java/ac/cnu/realcodingapi/domain/UserGroupDomainService.java
@@ -1,19 +1,24 @@
 package ac.cnu.realcodingapi.domain;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 public class UserGroupDomainService {
 
     private final UserGroupRepository userGroupRepository;
 
+    @Autowired
     public UserGroupDomainService(UserGroupRepository userGroupRepository) {
         this.userGroupRepository = userGroupRepository;
     }
 
     @Transactional(readOnly = true)
     public UserGroup getUserGroup(long id) {
+        log.info("Domain Service 함수 시작");
         return userGroupRepository.findById(id)
                 .orElseGet(() -> new UserGroup("no group"));
     }
