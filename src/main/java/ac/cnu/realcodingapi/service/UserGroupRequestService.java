2c7f283f89876e17f414b39753fefabdc075a1d8
inspire12 <ox4443@naver.com>
2022-04-18 14:19:42 +0900

    develop - resttemplate exception 처리 추가

--- src/main/java/ac/cnu/realcodingapi/service/UserGroupRequestService.java
+++ src/main/java/ac/cnu/realcodingapi/service/UserGroupRequestService.java
@@ -3,12 +3,14 @@ package ac.cnu.realcodingapi.service;
 import ac.cnu.realcodingapi.config.ExternInfoConfig;
 import ac.cnu.realcodingapi.dto.UserGroupResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserGroupRequestService {
@@ -17,11 +19,16 @@ public class UserGroupRequestService {
 
     public UserGroupResponse getTeamMember(Long id){
         String url = externInfoConfig.getUrl() + "?id=" + id;
-        ResponseEntity<UserGroupResponse> response
-                = restTemplate.getForEntity(url, UserGroupResponse.class);
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            return new UserGroupResponse(id, "no group", new ArrayList<>());
+        try {
+            ResponseEntity<UserGroupResponse> response
+                    = restTemplate.getForEntity(url, UserGroupResponse.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                return new UserGroupResponse(id, "no group", new ArrayList<>());
+            }
+            return response.getBody();
+        } catch (Exception e) {
+            log.error("external api request error: {}", url, e);
         }
-        return response.getBody();
+        return new UserGroupResponse(id, "no group", new ArrayList<>());
     }
 }
